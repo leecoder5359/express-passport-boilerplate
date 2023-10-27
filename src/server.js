4c8578f82d987b73842b9eed845b4fd66e8ceb06
이호준 <leecoder5359@gmail.com>
2023-10-27 12:11:09 +0900

    (feat): static 경로 추가

--- src/server.js
+++ src/server.js
@@ -1,17 +1,19 @@
 import express from "express";
 import mongoose from "mongoose";
+import * as path from "path";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use("/static", express.static(path.join(path.resolve(), "public")));
 
 const port = 3000;
 app.listen(port, () => {
     console.log(`Listening on ${port}...`);
 });
 
-mongoose.connect('mongodb://localhost:27017/passport')
-    .then(() => console.log('mongodb connected'))
-    .catch((err) => console.log('err'));
+mongoose.connect("mongodb://localhost:27017/passport")
+    .then(() => console.log("mongodb connected"))
+    .catch((err) => console.log("err"));
 
