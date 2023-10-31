ca91c5db9923445189ce1771e4eef2f8f40902ab
이호준 <leecoder5359@gmail.com>
2023-10-31 10:19:37 +0900

    (feat): errorMiddleware 추가

--- src/server.js
+++ src/server.js
@@ -8,6 +8,7 @@ import { regenerate } from "./middleware/session/regenerate.middleware.js";
 import { authRouter } from "./routes/auth.router.js";
 import { indexRouter } from "./routes/index.router.js";
 import passport from "passport";
+import { ErrorMiddleware } from "./middleware/error.middleware.js";
 
 
 const app = express();
@@ -36,6 +37,8 @@ app.use("/auth", authRouter);
 //view engin setup
 app.set("view engine", "ejs");
 app.set("views", path.join(path.resolve(), "src", "views"));
+
+app.use(ErrorMiddleware);
 
 const port = PORT;
 app.listen(port, () => {
---
+++ src/utils/async.util.js
@@ -0,0 +1,1 @@
+export const wrapAsync = (func) => (req, res, next) => func(req, res, next).catch(next);
